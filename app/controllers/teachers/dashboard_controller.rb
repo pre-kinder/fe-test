bbc84f7eb845ae46eff731af956a7ce515019968
iPowers <isika.powers@gmail.com>
2021-11-03 14:41:48 -0600

    Create teachers dashboard controller.

---
+++ app/controllers/teachers/dashboard_controller.rb
@@ -0,0 +1,9 @@
+class Teachers::DashboardController < ApplicationController
+  def show
+    if params[:q].present?
+      @forecast = ForecastFacade.get_forecast(params[:q])
+    else
+      flash[:error] = 'Please enter a valid city'
+    end
+  end
+end
