20360d2341a89a38d80b4775b9c03d83b527ddca
Ezzedine Alwafai <ezzowafai@gmail.com>
2021-11-03 11:40:33 -0500

    Clean up sessions create

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -2,20 +2,9 @@ class SessionsController < ApplicationController
   # skip_before_action :authenticate_user
 
   def create
-    # auth_hash = request.env['omniauth.auth']
-    # name = auth_hash['info']['name'],
-    # email = auth_hash['info']['email'],
-    # google_id = auth_hash['uid'],
-    # token = auth_hash['credentials']['token']
-
-    # user = User.find_or_create_by(email: email)
-    # user.update(google_id: google_id, token: token)
-
     user = User.find_or_create_by(email: user_params[:email])
     session[:user_id] = user.id
-    # require "pry"; binding.pry
     redirect_to parent_dashboard_path
-    # redirect_to dashboard_path
   end
 
   def destroy
