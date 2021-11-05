bc1f9ecea7f01e176a82d63bbb5f35f509863063
iPowers <isika.powers@gmail.com>
2021-11-05 16:49:59 -0600

    Add routes.

--- config/routes.rb
+++ config/routes.rb
@@ -3,13 +3,24 @@ Rails.application.routes.draw do
   devise_for :users, controllers: { omniauth_callbacks: 'users/omniauth_callbacks',
     registrations: 'users/registrations' }
 
-  devise_scope :user do
-    get '/users/auth/:provider/callback', to: 'users/sessions#create'
-  end
+  # devise_scope :user do
+  #   get '/users/auth/:provider/callback', to: 'users/sessions#create'
+  # end
 
   get '/register', to: 'registrations#new'
 
   root 'welcome#index'
+
+  namespace :teachers do
+    get '/dashboard', to: 'dashboard#index'
+    get '/register', to: 'registration#new'
+    post '/register', to: 'registration#create'
+    get '/profile', to: 'dashboard#show'
+    get '/profile/edit', to: 'dashboard#edit'
+  end
+
+  resources :parents
+
   # get '/auth/:provider/callback', to: 'sessions#create'
   # post '/', to: 'sessions#create'
   # delete '/logout', to: 'sessions#destroy'
@@ -22,14 +33,4 @@ Rails.application.routes.draw do
   # get '/teachers/register', to: 'teachers#new'
   # get '/teachers/:id/dashboard', to: 'teachers#show', as: 'teachers_dashboard'
   # get '/teachers/:id/profile', to: 'teachers#edit', as: 'teachers_profile'
-
-  namespace :teachers do
-    get '/dashboard', to: 'dashboard#index'
-    get '/register', to: 'registration#new'
-    post '/register', to: 'registration#create'
-    get '/profile', to: 'dashboard#show'
-    get '/profile/edit', to: 'dashboard#edit'
-  end
-
-  resources :parents
 end
