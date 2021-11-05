8bb24fba0e437c64808475d57385b167fc655a4b
iPowers <isika.powers@gmail.com>
2021-11-05 13:46:03 -0600

    added namespace for teachers controller and views

--- app/controllers/teachers/dashboard_controller.rb
+++ app/controllers/teachers/dashboard_controller.rb
@@ -1,9 +1,24 @@
 class Teachers::DashboardController < ApplicationController
-  def show
+  before_action :check_teacher_role?
+  def index
     if params[:q].present?
       @forecast = ForecastFacade.get_forecast(params[:q])
     else
       flash[:error] = 'Please enter a valid city'
     end
+  end
+
+  def show
+
+  end
+
+  def create
+  end
+
+  def edit
+  end
+
+  def check_teacher_role?
+    current_user.role == 'teacher'
   end
 end
