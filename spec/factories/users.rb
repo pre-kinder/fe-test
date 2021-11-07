81d23dce93ec62a740fd1ae90b388160bf7027d0
iPowers <isika.powers@gmail.com>
2021-11-06 20:44:09 -0600

    Add users to factories.

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,4 +1,16 @@
 FactoryBot.define do
+  factory :mock_user, class: User do
+    email { Faker::Internet.email }
+    first_name { Faker::Name.first_name }
+    last_name { Faker::Name.last_name }
+    phone_number { Faker::PhoneNumber }
+    address { Faker::Address }
+    role { }
+    google_id { 'jdfhiewlej83hr'}
+    token { 'kdjfdlsgkirjeg;weskdr3u9843' }
+    password { Faker::Internet.password(min_length: 6) }
+  end
+
   factory :mock_teacher, class: User do
     email { Faker::Internet.email }
     first_name { Faker::Name.first_name }
