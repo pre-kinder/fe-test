692ccf63859e4ad7558c8baf996fcf031ae382c2
iPowers <isika.powers@gmail.com>
2021-11-06 20:47:25 -0600

    Create tests for welcome page.

--- spec/features/welcome/index_spec.rb
+++ spec/features/welcome/index_spec.rb
@@ -1,10 +1,28 @@
 require 'rails_helper'
 
 RSpec.describe 'Welcome page' do
-  it 'displays welcome home page' do
-    visit root_path
+  before :each do
+    Rails.application.env_config["devise.mapping"] = Devise.mappings[:user]
+    Rails.application.env_config["omniauth.auth"] = OmniAuth.config.mock_auth[:google_oauth2]
 
-    expect(page).to have_content('Welcome to PreKinder')
-    expect(page).to have_link('Sign in with Google')
+    @user = create(:mock_user)
+
+    allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(@user)
+  end
+
+  context "log in with Google" do
+    it 'displays new teacher form for a new user after logged in with Google' do
+      visit root_path
+
+      find(:xpath, "//a/img[@alt='Google sign in button']/..").click
+
+      sleep 1
+
+      expect(current_path).to eq (root_path)
+
+      expect(page).to have_content("Welcome, #{@user.first_name}!")
+      expect(page).to have_link('New Teacher? Register an account here')
+      expect(page).to have_link('New Parent? Register an account here')
+    end
   end
 end
