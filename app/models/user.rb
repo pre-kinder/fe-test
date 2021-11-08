c8edf0eb366bda8e9adbba9f4bc7a879feeb6b54
iPowers <isika.powers@gmail.com>
2021-11-08 11:15:21 -0700

    Add timeoutable feature to login.

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,7 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable,
+         :timeoutable,
          :omniauthable, omniauth_providers: [:google_oauth2]
 
   validates :email, presence: true, uniqueness: true
