239ed5605574dd2c2415b01de925bd7dc01a7d59
Jacq Whitmer <jacqwhitmer@gmail.com>
2021-11-11 15:19:11 -0800

    Fix lassroom poro testing

--- spec/poros/classroom_spec.rb
+++ spec/poros/classroom_spec.rb
@@ -4,19 +4,12 @@ RSpec.describe Classroom do
   it "exists and has attributes" do
     main_attr = {
       "name": "Room 103",
-      "children": ["Billy Burton", "Pat Smith", "Jaime Jones"],
-      "teachers": ["Mr. Jones-Nme", "Ms. Williams"],
-      "events": ["Teacher planning day", "Veterans Day"]
+      "id": 1
     }
     @class = Classroom.new(main_attr)
 
     expect(@class).to be_a Classroom
     expect(@class.name).to eq("Room 103")
-    expect(@class.children).to be_an Array
-    expect(@class.children).to eq(["Billy Burton", "Pat Smith", "Jaime Jones"])
-    expect(@class.teachers).to be_an Array
-    expect(@class.teachers).to eq(["Mr. Jones-Nme", "Ms. Williams"])
-    expect(@class.events).to be_an Array
-    expect(@class.events).to eq(["Teacher planning day", "Veterans Day"])
+    expect(@class.id).to eq(1)
   end
 end
