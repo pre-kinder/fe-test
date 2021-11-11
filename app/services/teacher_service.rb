1fe1fc26284594d536edd4bd18035184548ecf82
Jacq Whitmer <jacqwhitmer@gmail.com>
2021-11-10 16:48:52 -0800

    Fix teacher service methods

--- app/services/teacher_service.rb
+++ app/services/teacher_service.rb
@@ -1,23 +1,21 @@
 class TeacherService < BackEndService
-  def self.get_one_teacher(google_id)
-    response = conn.get("/api/v1/teacher?#{google_id}")
-    
+  def self.get_one_teacher(email)
+    response = conn.get("/api/v1/teacher?#{email}")
+
     parse_json(response)
   end
 
   def self.create_teacher(teacher_params)
     conn.post(
       '/api/v1/teachers',
-      teacher_params.to_json,
-      'Content-Type' => 'application/json'
+      teacher_params
     )
   end
 
-  def self.update_teacher_profile(teacher_params, google_id)
+  def self.update_teacher_profile(teacher_params, teacher_id)
     conn.patch(
-      "/api/v1/teachers/#{google_id}",
-       teacher_params.to_json,
-      'Content-Type' => 'application/json'
+      "/api/v1/teachers/#{teacher_id}",
+       teacher_params
     )
   end
 end
