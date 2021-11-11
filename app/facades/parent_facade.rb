23b626d18cd0b502b009435bb736ff3e5afc870c
danembb <dbrophy720@gmail.com>
2021-11-11 12:34:54 -0700

    Update parent_facade testing

--- app/facades/parent_facade.rb
+++ app/facades/parent_facade.rb
@@ -1,10 +1,9 @@
 class ParentFacade
   def self.get_one_parent(google_id)
     parent_data = ParentService.request_api(google_id)
-
     Parent.new(parent_data[:data][:attributes])
   end
-  
+
   def self.post_parent(parent_params)
     ParentService.post_parent_to_api(parent_params)
   end
