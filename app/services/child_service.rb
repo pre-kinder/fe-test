72fdf31b14913f588542af791ee076aa9e5217e3
Jacq Whitmer <jacqwhitmer@gmail.com>
2021-11-10 16:55:28 -0800

    Fix child service methods

--- app/services/child_service.rb
+++ app/services/child_service.rb
@@ -1,9 +1,9 @@
 class ChildService < BackEndService
-  def self.get_one_child(last_name, birthday)
-    response = conn.get("/api/v1/child?last_name=#{last_name}&birthday=#{birthday}")
-
-    parse_json(response)
-  end
+  # def self.get_one_child(last_name, birthday)
+  #   response = conn.get("/api/v1/child?last_name=#{last_name}&birthday=#{birthday}")
+  #
+  #   parse_json(response)
+  # end
 
   def self.get_classroom_children(classroom_id)
     response = conn.get("/api/v1/classrooms/#{classroom_id}/children")
@@ -20,8 +20,7 @@ class ChildService < BackEndService
   def self.create_child(child_params)
     conn.post(
       '/api/v1/children',
-      child_params.to_json,
-      'Content-Type' => 'application/json'
+      child_params
     )
   end
 end
