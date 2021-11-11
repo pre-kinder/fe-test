e8fde4e8e56ba72ee270f0b20878107d80c7dd91
danembb <dbrophy720@gmail.com>
2021-11-11 16:43:17 -0700

    Delete parent model file

--- app/models/parent.rb
+++
@@ -1,7 +0,0 @@
-class Parent < ApplicationRecord
-  has_many :children
-  
-  validates :email, presence: true, uniqueness: true
-  validates :first_name, :last_name, presence: true
-  validates :phone_number, :address, presence: true
-end
